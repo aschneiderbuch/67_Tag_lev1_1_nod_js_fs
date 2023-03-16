3e7d28a5a5f48d41beca040abd8d25a8c39771ba
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-03-16 13:39:08 +0100

    3te

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fs, { copyFileSync } from "fs"
 
+// in blog1.txt steht bereits "Hello World"
 fs.open("./blog1.txt", (err)=>{
     if(err){
         console.log("Error beim fs.open" , err)
@@ -10,8 +11,22 @@ fs.open("./blog1.txt", (err)=>{
 })
 
 // ändere den Text von blog1.txt
-fs.write("./blog1.txt" , "input" , (err)=> {
+fs.writeFile( "./blog1.txt", "Guten Morgen" ,   (err)=> {
     if (err){
         console.log("Error bei fs.write ", err)
     }
+    else{
+        console.log("Schreiben in fs.writeFile Guten Morgen hat geklappt")
+    }
 })
+
+// erstelle neue blog2.txt  und beliebigen text rein
+fs.writeFile("./blog2.txt" , "Da ist das 2te txt Ding"  , (err)=>{
+    if(err){
+        console.log("Error in fs.writeFile 2te.txt " , err)
+    }
+    else {
+        console.log("2te fs.writeFile läuft " )
+    }
+})
+
